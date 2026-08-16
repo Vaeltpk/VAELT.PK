dcdc246675fd970e0ab29dea8f2b1c642d9b246c
Vaeltpk <Vaelt.pk@gmail.com>
2026-08-16 15:33:26 -0700

    Update products.js

--- products.js
+++ products.js
@@ -30,7 +30,7 @@ const products = [
     size: "UK 9",
     condition: "Excellent",
     image: "images/nike-af1.jpg",
-    gallery: ["images/nike-af1.jpg"],
+    gallery: ["images/nike-af1.jpg","images/"],
     description: "Preloved Nike running shoes in excellent condition. Clean upper with comfortable cushioning and good outsole grip."
   },
   {
